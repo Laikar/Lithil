171a30683e979510c9aea11c9f25824709488d33
David <david@laikar.moe>
2020-01-15 18:03:23 +0100

    CREO QUE ESTAMOS DAIJOBU

--- lithil.sh
+++ lithil.sh
@@ -12,14 +12,10 @@ USER_NAME="lithil"
 # Setup the default installation location.
 LOCATION="/opt/lithil"
 
-# Setup the arguments to the lictl script.
-# shellcheck disable=SC2124
-LITHIL_ARGS="-p $PROG -l $LOCATION -c $MSCS_DEFAULTS $@"
-
 # Run the lictl script.
 if [ "$USER_NAME" = "$(whoami)" ]; then
   lictl "$LITHIL_ARGS"
 else
-  echo "sudo -u ${USER_NAME} -H lictl $LITHIL_ARGS"
-  sudo -u ${USER_NAME} -H lictl "$LITHIL_ARGS"
+  echo "sudo -u ${USER_NAME} -H lictl $@"
+  sudo -u ${USER_NAME} -H lictl "$@"
 fi
