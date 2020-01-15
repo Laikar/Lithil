a30c1b8c7462bb2c4ed7c3a81b7329600ef61068
David <david@laikar.moe>
2020-01-15 22:52:51 +0100

    creo que siiii

--- internals/lithil_client.py
+++ internals/lithil_client.py
@@ -35,7 +35,8 @@ class LithilClient(discord.Client):
         self.watching_voice_channels = False
         self.process_pool = ThreadPoolExecutor(5)
         try:
-            self.loop.add_signal_handler(signal.SIGTERM, self.loop.run_in_executor(self.process_pool, self.stop_bot))
+            self.loop.add_signal_handler(signal.SIGTERM,
+                                         lambda: self.loop.run_in_executor(self.process_pool, self.stop_bot))
         except NotImplementedError:
             pass
 
